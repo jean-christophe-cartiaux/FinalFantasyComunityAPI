d8c504a141c57ce334c41e6d77e149886181b315
jean-christophe-cartiaux <cartiaux.jc@hotmail.com>
2024-06-12 09:32:36 +0200

    finition des relations

--- src/reactions/entities/reaction.entity.ts
+++ src/reactions/entities/reaction.entity.ts
@@ -1,4 +1,7 @@
-import {Column, PrimaryGeneratedColumn} from "typeorm";
+import {Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Utilisateurs} from "../../utilisateurs/entities/utilisateur.entity";
+import {PublicationForum} from "../../publication-forums/entities/publication-forum.entity";
+import {Actualites} from "../../actualites/entities/actualite.entity";
 
 export class Reaction {
 
@@ -13,4 +16,18 @@ export class Reaction {
 
 
     //TODO FK ID UTILISATEUR  / ID PUBLICATION
+
+    @ManyToOne(()=>Utilisateurs)
+    @JoinColumn({name:"idUtilisateur"})
+    utilisateur:Utilisateurs;
+
+    @ManyToOne(()=>PublicationForum)
+    @JoinColumn({name:"idPublication"})
+    publication:PublicationForum;
+
+    @ManyToOne(()=>Actualites)
+    @JoinColumn({name:"idActu"})
+    actualite:Actualites;
+
+
 }
--- src/signalement/entities/signalement.entity.ts
+++ src/signalement/entities/signalement.entity.ts
@@ -1,4 +1,6 @@
-import {Column, PrimaryGeneratedColumn} from "typeorm";
+import {Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Utilisateurs} from "../../utilisateurs/entities/utilisateur.entity";
+import {PublicationForum} from "../../publication-forums/entities/publication-forum.entity";
 
 export class Signalement {
 
@@ -13,4 +15,16 @@ export class Signalement {
 
     // TODO
     //  FK ID SIGNALEUR  / ID UTILISATEUR SIGNALER // ID PUBLICATION SIGNALER
+
+    @ManyToOne(() => Utilisateurs)
+    @JoinColumn({ name: "idSignaleur" })
+    signaleur: Utilisateurs;
+
+    @ManyToOne(() => Utilisateurs)
+    @JoinColumn({ name: "idUtilisateurSignaler" })
+    utilisateurSignale: Utilisateurs;
+
+    @ManyToOne(() => PublicationForum)
+    @JoinColumn({ name: "idPublicationSignaler" })
+    publicationSignale: PublicationForum;
 }
